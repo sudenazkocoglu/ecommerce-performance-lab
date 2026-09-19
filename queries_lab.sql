8f3aef884340ca3d8d86321a500083a01ddcafc7
Sudenaz Koçoğlu <sudenazkocoglu4@gmail.com>
2026-09-20 02:20:05 +0300

    Hocanın geri bildirimleri doğrultusunda performans ölçümleri ve SCD2 kanıtı eklendi.

--- queries_lab.sql
+++ queries_lab.sql
@@ -1,4 +1,10 @@
--- Adım 3: Kanıtı görmek için müşteriyi sorguluyoruz
-SELECT customer_id, first_name, city, is_current, valid_from, valid_to 
-FROM dim_customer 
-WHERE customer_id = 1;
+-- Soru 10 OLTP
+EXPLAIN (ANALYZE, BUFFERS)
+SELECT DATE(created_at) AS siparis_tarihi, AVG(total_amount) AS gunluk_ortalama 
+FROM orders GROUP BY DATE(created_at) ORDER BY siparis_tarihi;
+
+-- Soru 10 STAR
+EXPLAIN (ANALYZE, BUFFERS)
+SELECT dd.full_date, AVG(fo.total_amount) AS gunluk_ortalama 
+FROM fct_orders fo JOIN dim_date dd ON fo.date_key = dd.date_key 
+GROUP BY dd.full_date ORDER BY dd.full_date;
